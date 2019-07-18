4d2d46bdc6bba67301173d5a8c47f603cfe576e5
seoyun <sallylucky@naver.com>
2019-07-18 13:01:49 +0900

    Login_signup_email
    +firebase

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import firebase from 'firebase'
+
 Vue.config.productionTip = false
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBv2nclaViHPFgmZuuogaJht0o7rbxfuB8",
+    authDomain: "kagosim-meow.firebaseapp.com",
+    databaseURL: "https://kagosim-meow.firebaseio.com",
+    projectId: "kagosim-meow",
+    storageBucket: "",
+    messagingSenderId: "436058054311",
+    appId: "1:436058054311:web:68e13fc17dea2f23"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
