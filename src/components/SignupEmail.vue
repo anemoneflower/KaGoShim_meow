829d7253a1ca67997d4957db87b6fa9944269dad
seoyun <sallylucky@naver.com>
2019-07-19 18:04:30 +0900

    Convert login page to signup_email

---
+++ src/components/SignupEmail.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="signupEmail">
+        <h4>이메일이다냥</h4>
+        <input type="text">
+        <h4>비밀번호6자리이상이다냥</h4>
+        <input type="text">
+        <div>
+            <button @click="SignUp()">가입하냥?</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+    name: 'SignupEmail',
+    data(){
+        return{
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        SignUp(){
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+                .then((user) => {
+                    console.log(user)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+h4{
+    margin: 40px;
+}
+</style>
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="login">
-    <h1>This is an login page</h1>
+    <SignupEmail/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import SignupEmail from '@/components/SignupEmail.vue'
+
+export default {
+  name: 'login',
+  components: {
+    SignupEmail
+  }
+}
+</script>
